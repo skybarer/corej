9e9a75a0f902fe1c4dc4a491eaae1dbb99a89555
Inkollu Akashdhar <inkollu.a@HCL.COM>
2018-10-16 12:10:11 +0530

    Intial commit

--- src/oops/InterfaceTest.java
+++ src/oops/InterfaceTest.java
@@ -1,20 +1,21 @@
 package oops;
 
-
 // only public and abstract are allowed
 public abstract interface InterfaceTest {
-	
-	//<T> allows generic types
+
+	// <T> allows generic types
 
 	// public static final variables are allowed
 	public static final int a = 0;
-	
-	int b =4; 
-	
-	//Interfaces cannot have constructors
-	/*public InterfaceTest() {
-		
-	}*/
+
+	int b = 4;
+
+	// Interfaces cannot have constructors
+	/*
+	 * public InterfaceTest() {
+	 * 
+	 * }
+	 */
 
 	public default void defaultMethod() {
 		// default methods are allowed
@@ -24,10 +25,35 @@ public abstract interface InterfaceTest {
 		// static methods are allowed
 	}
 
+	// subclass
+
+	// The interface member type SubClassTest can only be public
+	// The class SubClassTest can be either abstract or final, not both
+	public static abstract class SubClassTest {
+
+		public static void main(String[] args) {
+
+			System.out.println("main method in sub class");
+		}
+
+	}
+
+	// sub interface
+	interface SubInterfaceTest {
+
+	}
+
+	// sub enum
+	enum EnumTest {
+
+		RED, GREEN, BLUE
+
+	}
+
 	public static void main(String[] args) {
 
 		System.out.println("interface main method");
-		
+
 		System.out.println(InterfaceTest.a);
 		System.out.println(InterfaceTest.a);
 	}
