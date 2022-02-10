e12e3a33138b9bae264bec82e1d83c4678f56ec0
skybarer <akashdhar.apssdc@gmail.com>
2022-02-10 12:41:18 +0530

    modified to lambda expression

--- src/__examples/ShutDownHook.java
+++ src/__examples/ShutDownHook.java
@@ -4,11 +4,7 @@ public class ShutDownHook {
 
 	public static void main(String[] args) {
 
-		Runtime.getRuntime().addShutdownHook(new Thread() {
-			public void run() {
-				System.out.println("Shutdown Hook is running !");
-			}
-		});
+		Runtime.getRuntime().addShutdownHook(new Thread(() -> System.out.println("Shutdown Hook is running !")));
 
 		System.out.println("Application Terminating ");
 	}
